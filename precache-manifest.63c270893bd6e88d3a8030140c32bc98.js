f1a8707c907a5acee4365fb5c82140bece5cc1cb
Vincent Nguyen <vincenttsnguyen@gmail.com>
2021-01-07 16:41:47 -0800

    Updates

--- precache-manifest.63c270893bd6e88d3a8030140c32bc98.js
+++ precache-manifest.63c270893bd6e88d3a8030140c32bc98.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "bf47b21f52ca6b368546",
-    "url": "/portfolio/static/js/main.bf47b21f.chunk.js"
+    "revision": "cb09a049a9ead04cfecd",
+    "url": "/portfolio/static/js/main.cb09a049.chunk.js"
   },
   {
     "revision": "1048638a4991ffad96d3",
     "url": "/portfolio/static/js/1.1048638a.chunk.js"
   },
   {
-    "revision": "bf47b21f52ca6b368546",
+    "revision": "cb09a049a9ead04cfecd",
     "url": "/portfolio/static/css/main.a773ec76.chunk.css"
   },
   {
-    "revision": "4c4430a76f4c202d966ecdb6cfd16c07",
+    "revision": "63d462eb50c9e0d0e88470fda4f7000b",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b18e42c36181de2385b29af52cd9b9f8.js"
+  "/portfolio/precache-manifest.63c270893bd6e88d3a8030140c32bc98.js"
 );
 
 workbox.clientsClaim();
